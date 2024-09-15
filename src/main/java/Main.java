1b2b0ce8bc1793573b765f328d23a3df45377c8c
KimS08 <kimstrugo2@gmail.com>
2024-09-15 17:55:02 +0300

    Add files via upload

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+	static Scanner input = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		
+		System.out.print("Enter length then width: ");
+		int l= input.nextInt();
+		int w = input.nextInt();
+		Rectangle rec = new Rectangle(l, w);
+		
+		System.out.print("Enter length then width: ");
+		int length = input.nextInt();
+		int width = input.nextInt();
+		Rectangle rec2 = new Rectangle(length, width);
+		
+		rec.draw(rec.getLength(), rec.getWidth());
+		rec2.draw(rec2.getLength(), rec2.getWidth());
+		
+		rec.scale(2);
+		
+		int p1 =rec.calcPerimeter(rec.getLength(), rec.getWidth());
+		int p2 =rec2.calcPerimeter(rec2.getLength(), rec2.getWidth());
+
+		System.out.println("The perimeter sum is: " + (p1 + p2));
+		
+		System.out.println(rec);
+		System.out.println(rec2);
+
+	}
+}
